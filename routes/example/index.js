1d941e5cc6199399c55d019eff40cfe05132f8b3
Cormorant Programming <cormorant.programming@gmail.com>
2022-07-13 22:27:58 +0700

    routes

--- routes/example/index.js
+++ routes/example/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
+
   fastify.get('/', async function (request, reply) {
     return 'this is an example'
   })
+  
+  fastify.get('/:id', async function (request, reply) {
+    return request.params
+  })
+
 }
